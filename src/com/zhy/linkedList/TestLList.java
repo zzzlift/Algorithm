7998bc36d1432ca58f82c3c2e4a2f05bfd6d851e
zzzlift <zhyxhys@163.com>
2016-07-02 22:29:24 +0800

    update

--- src/com/zhy/linkedList/TestLList.java
+++ src/com/zhy/linkedList/TestLList.java
@@ -19,11 +19,27 @@ public class TestLList {
 	}
 	@Test
 	public void testDoublelyLList(){
-		DoublelyLList l=new DoublelyLList();
+		DoublyLList l=new DoublyLList();
 		l.push(1);
 		l.push(2);
 		l.display();
 		l.dispalyReverse();
+		System.out.println("insert");
+		l.insert(3,1);
+		System.out.println("display");
+		l.display();
+		System.out.println("remove");
+		l.remove(3);
+		l.display();
+		System.out.println("find");
+		System.out.println(l.find(3));
+	}
+	@Test
+	public void testCList(){
+		CircularlyLList l=new CircularlyLList();
+		l.push(1);
+		l.push(2);
+		l.display();
 		System.out.println("insert");
 		l.insert(3,1);
 		System.out.println("display");
