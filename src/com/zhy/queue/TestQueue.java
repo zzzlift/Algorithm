028500ba3e3d21574a9cea65c0299c05b852c730
zzzlift <zhyxhys@163.com>
2016-07-03 23:00:17 +0800

    update

--- src/com/zhy/queue/TestQueue.java
+++ src/com/zhy/queue/TestQueue.java
@@ -6,17 +6,30 @@ public class TestQueue {
 	@Test
 	public void testQueue(){
 		Queue q =new Queue(5);
-		q.push(1);
-		q.push(2);
-		q.push(3);
-		q.push(4);
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		q.enqueue(4);
 		q.display();
 		System.out.println(q.peek());
 		System.out.println(q.end());
 		System.out.println("=====");
-		System.out.println(q.shift());
+		System.out.println(q.dequeue());
 		System.out.println("=====");
 		q.display();
 		
+	}
+	
+	@Test
+	public void testPQueue(){
+		MyPriorityQueue q=new MyPriorityQueue(5);
+		q.enqueue(5,5);
+		q.enqueue(3, 3);
+		q.enqueue(6, 6);
+		q.enqueue(4, 4);
+		q.display();
+		q.dequeue();
+		System.out.println("--");
+		q.display();
 	}
 }
