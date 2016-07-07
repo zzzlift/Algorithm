f858ed13b92605028e5be953e10dcc439217ad34
zzzlift <zhyxhys@163.com>
2016-07-07 17:05:57 +0800

    fix bucketsort

--- src/com/zhy/sort/BucketSort.java
+++ src/com/zhy/sort/BucketSort.java
@@ -3,31 +3,14 @@ package com.zhy.sort;
 import java.util.Arrays;
 
 public class BucketSort {
-	public int[] bucketSort(int[] a,int k){
-		int[] c=new int[k+1];
-		int[] b=new int[a.length+1];
-		for(int i=0;i<=k;i++){
-			c[i]=0;
-		}
-		for(int j=0;j<a.length;j++){
-			c[a[j]]=c[a[j]]+1;
-		}
-		for(int i=1;i<=k;i++){
-			c[i]=c[i]+c[i-1];
-		}
-		for(int j=a.length-1;j>=0;j--){
-			b[c[a[j]]]=a[j];
-			c[a[j]]=c[a[j]]-1;
-		}
-		//注意最终的b[0]是一个多余的0，要干掉
-		b=Arrays.copyOfRange(b, 1, b.length);
-		return b;
+	public int[] bucketSort(int[] a){
+		return null;
 	}
 	
 	public static void main (String args[]){
 		int[] a={2,4,1,5,7,0,3,2,3};
 		BucketSort bu=new BucketSort();
-		int[] b=bu.bucketSort(a,7);
+		int[] b=bu.bucketSort(a);
 		System.out.println(Arrays.toString(b));
 	}
 }
