ffb19f34a8cca4d9c92090525a6c8818cb12586e
zzzlift <zhyxhys@163.com>
2016-07-07 17:08:41 +0800

    update counting sort

--- src/com/zhy/sort/CountingSort.java
+++ src/com/zhy/sort/CountingSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CountingSort {
 	public int[] countingSort(int[] a,int k){
 		int[] c=new int[k+1];
-		int[] b=new int[a.length+1];
+		int[] b=new int[a.length];
 		for(int i=0;i<=k;i++){
 			c[i]=0;
 		}
@@ -16,11 +16,11 @@ public class CountingSort {
 			c[i]=c[i]+c[i-1];
 		}
 		for(int j=a.length-1;j>=0;j--){
-			b[c[a[j]]]=a[j];
-			c[a[j]]=c[a[j]]-1;
+			b[--c[a[j]]]=a[j];
+//			c[a[j]]=c[a[j]]-1;
 		}
-		//注意最终的b[0]是一个多余的0，要干掉
-		b=Arrays.copyOfRange(b, 1, b.length);
+//		//注意最终的b[0]是一个多余的0，要干掉
+//		b=Arrays.copyOfRange(b, 1, b.length);
 		return b;
 	}
 	
