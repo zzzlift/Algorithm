2f825ea0017a3d1bc33a78b774d3fd1b1c19edae
zzzlift <zhyxhys@163.com>
2016-07-07 17:02:29 +0800

    update radixSort

--- src/com/zhy/sort/RadixSort.java
+++ src/com/zhy/sort/RadixSort.java
@@ -1,5 +1,37 @@
 package com.zhy.sort;
 
-public class RadixSort {
+import java.util.Arrays;
 
+public class RadixSort {
+	public void radixSort(int[] a){
+		int i, m = a[0], exp = 1, n = a.length;
+        int[] b = new int[10];
+        for (i = 1; i < n; i++)
+            if (a[i] > m)
+                m = a[i];
+        while (m / exp > 0)
+        {
+            int[] bucket = new int[10];
+ 
+            for (i = 0; i < n; i++)
+            	//把每个元素的个位放进bucket来计数[1, 2, 2, 2, 0, 0, 0, 1, 1, 0]，用的计数排序的思想
+                bucket[(a[i] / exp) % 10]++;
+            //将数量进行累加[1, 3, 5, 7, 7, 7, 7, 8, 9, 9]
+            for (i = 1; i < 10; i++)
+                bucket[i] += bucket[i - 1];
+            for (i = n - 1; i >= 0; i--)
+                b[--bucket[(a[i] / exp) % 10]] = a[i];
+            //然后改变a为第一次排列后的值
+            for (i = 0; i < n; i++)
+                a[i] = b[i];
+            exp *= 10;        
+        }
+	}
+	
+	public static void main (String args[]){
+		int[] a={48,32,1,51,7,20,3,2,3};
+		RadixSort r=new RadixSort();
+		r.radixSort(a);
+		System.out.println(Arrays.toString(a));
+	}
 }
